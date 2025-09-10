67a5ddefb808e85aced74c593e426fa9707dcdf9
jinrecatdo <139969873+jinrecatdo@users.noreply.github.com>
2025-09-11 06:30:59 +0700

    java

---
+++ demon/src/main/java/com/mysql/demon/Dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.mysql.demon.Dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao {
+
+
+}
---
+++ demon/src/main/java/com/mysql/demon/entity/User.java
@@ -0,0 +1,4 @@
+package com.mysql.demon.entity;
+
+public class User {
+}
